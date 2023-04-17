4d0a19e6b6aba7e249dbd754a3cca7b23729e0f4
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-04-17 16:55:40 -0500

    database conection generated

--- src/models/server.ts
+++ src/models/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
 import { router } from '../routes/user.routes';
+import { dbConnection } from '../database/config.db';
 
 dotenv.config();
 
@@ -13,8 +14,13 @@ export class Server {
         this.app = express();
         this.port = process.env.PORT!;
         this.usuariosPath = '/api/usuarios';
+        this.createDBConection();
         this.middlewares();
         this.routes();
+    }
+
+    async createDBConection() {
+        await dbConnection();
     }
 
     middlewares() {
