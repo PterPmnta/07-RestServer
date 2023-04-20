a8484a9549cfe55dc4ebd7deeca26e917e1945d4
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-04-19 19:14:56 -0500

    express validator implemented

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -7,10 +7,27 @@ import {
     putUsers
 } from '../controllers/users.controller';
 import { check } from 'express-validator';
+import { userValidation } from '../middlewares/user-validator';
 
 export const router = Router();
 
-router.post('/', [check('email', 'Email is not valid').isEmail()], postUsers);
+router.post(
+    '/',
+    [
+        check('email', 'Email is not valid').isEmail(),
+        check('name', 'Name cant be empty').not().isEmpty(),
+        check('password', 'Password must be at least 6 characters')
+            .isLength({ min: 6 })
+            .not()
+            .isEmpty(),
+        check('role', 'This role is not valid').isIn([
+            'ADMIN_ROLE',
+            'USER_ROLE'
+        ]),
+        userValidation
+    ],
+    postUsers
+);
 
 router.get('/', getUsers);
 
