6a5453ec02759ce8b488ad482615bbb99f2ba312
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-04-20 19:26:13 -0500

    unnecesary code removed

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -4,17 +4,8 @@ import { UserModel } from '../models/user.model';
 import { validationResult } from 'express-validator';
 
 export const postUsers = async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors);
-    }
-
     const { name, email, password, role } = req.body;
     const user = new UserModel({ name, email, password, role });
-
-    const emailExtisted = await UserModel.findOne({ email });
-    if (emailExtisted)
-        return res.status(400).json({ msg: 'Email already exists' });
 
     const salt = bcrypt.genSaltSync(10);
     user.password = bcrypt.hashSync(password, salt);
