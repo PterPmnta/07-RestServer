829af889e6e3ea3c21ebfb20dc1ca47c29a11918
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-04-26 20:16:08 -0500

    login finished

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,43 @@
 import { Response, Request } from 'express';
+import bcrypt from 'bcryptjs';
+import { UserModel } from '../models/user.model';
 
-export const login = (req: Request, res: Response) => {
-    res.json({
-        msg: 'Login OK'
-    });
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+
+        const existedUser = await UserModel.findOne({ email });
+        if (!existedUser) {
+            return res.status(400).json({
+                msg: 'El usuario no existe'
+            });
+        }
+
+        if (!existedUser.status) {
+            return res.status(400).json({
+                msg: 'El usuario no esta activo'
+            });
+        }
+
+        const isValidPassword = bcrypt.compareSync(
+            password,
+            existedUser.password
+        );
+
+        if (!isValidPassword) {
+            return res.status(400).json({
+                msg: 'Credenciales de acceso incorrectas'
+            });
+        }
+
+        res.json({
+            msg: 'Login OK',
+            email,
+            password
+        });
+    } catch (error) {
+        return res.status(500).json({
+            msg: 'Error al intentar loguearse'
+        });
+    }
 };
