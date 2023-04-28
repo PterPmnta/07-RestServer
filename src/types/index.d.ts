976e6d78022bc822ac73867ae3f8680c4b4595b3
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-04-28 18:59:38 -0500

    new attribute added

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,10 +1,12 @@
 export {};
 import express from 'express';
+import { IUser } from '../interfaces/user';
 
 declare global {
     namespace Express {
         interface Request {
             uid: string;
+            user: IUser | null;
         }
     }
 }
