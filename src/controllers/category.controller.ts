d89d6018ed8590d199fb6bb6e0fff1b791c962f2
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-05-12 17:17:15 -0500

    function to get data created

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,34 +1,79 @@
 import { Response, Request } from 'express';
 import { CategoryModel } from '../models/category.model';
 
-export const createCategory = (req: Request, res: Response) => {
+export const createCategory = async (req: Request, res: Response) => {
     try {
         const name = req.body.name.toUpperCase();
 
-        const categoryNameValidate: any = CategoryModel.findOne({ name });
+        const categoryNameValidate = await CategoryModel.findOne({ name });
 
         if (categoryNameValidate) {
             return res.status(400).json({
-                msg: `Category ${categoryNameValidate.name} already exists`
+                msg: `Category ${categoryNameValidate} already exists`
             });
         }
 
         const data = {
             name,
-            usuario: req.user._id
+            user: req.user._id
         };
 
         const category = new CategoryModel(data);
-        category.save();
+        await category.save();
 
         res.status(201).json({
             msg: 'Category created successfully',
             category
         });
     } catch (error) {
-        console.log(error);
         res.status(500).json({
-            msg: 'Something went wrong'
+            msg: 'Something went wrong - POST Category'
         });
     }
 };
+
+export const getCategories = async (req: Request, res: Response) => {
+    try {
+        const { limite = 5, desde = 0 } = req.query;
+        const query = { status: true };
+
+        const [categories, numberCategories] = await Promise.all([
+            CategoryModel.find(query)
+                .limit(Number(limite))
+                .skip(Number(desde))
+                .populate('user', 'name'),
+            CategoryModel.countDocuments(query)
+        ]);
+
+        res.json({
+            categories,
+            numberCategories
+        });
+    } catch (error) {
+        res.status(500).json({
+            msg: 'Something went wrong - GET categories'
+        });
+    }
+};
+
+export const getCategoriesById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const category = await CategoryModel.findById(id).populate(
+            'user',
+            'name'
+        );
+
+        res.json({
+            category
+        });
+    } catch (error) {
+        res.status(500).json({
+            msg: 'Something went wrong - GET categories by ID'
+        });
+    }
+};
+
+export const updateCategories = () => {};
+
+export const disableCategory = () => {};
