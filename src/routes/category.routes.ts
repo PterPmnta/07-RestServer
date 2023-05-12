9a724cd075d534ee0a41f57a68d4dc5254514fb8
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-05-11 20:23:09 -0500

    validation and controller applied

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
+import { validateJWT } from '../middlewares/jwt-validator';
+import { createCategory } from '../controllers/category.controller';
 import { userValidation } from '../middlewares/user-validator';
 
 export const categoryRoutes = Router();
 
-categoryRoutes.post('/');
+categoryRoutes.post(
+    '/',
+    [
+        validateJWT,
+        check('name', 'Name is required').not().isEmpty(),
+        userValidation
+    ],
+    createCategory
+);
 
 categoryRoutes.get('/');
 
