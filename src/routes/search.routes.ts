8874cc874960f36fb5074025752e8e69eacec404
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-05-15 18:23:35 -0500

    route created

---
+++ src/routes/search.routes.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+import { searchDynamic } from '../controllers/search.controller';
+
+export const searchRoutes = Router();
+
+searchRoutes.get('/:collection/:term', searchDynamic);
