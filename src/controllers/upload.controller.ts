f1df4e2ba9d802de4a3c5b5901ccc04bb41dd275
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-05-23 16:52:06 -0500

    extention validation created

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -2,7 +2,10 @@ import { Response, Request } from 'express';
 import fileUpload from 'express-fileupload';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { v4 as uuidv4 } from 'uuid';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const extentionsAllowed = ['png', 'jpg', 'jpeg', 'gif'];
 
 export const uploadFile = async (req: Request, res: Response) => {
     try {
@@ -18,7 +21,17 @@ export const uploadFile = async (req: Request, res: Response) => {
 
         // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
         const fileUploaded = req.files.archivo as fileUpload.UploadedFile;
-        uploadPath = path.join(__dirname, '../uploads/', fileUploaded.name);
+        const splitName = fileUploaded.name.split('.');
+        const fileExtension = splitName[splitName.length - 1];
+
+        if (!extentionsAllowed.includes(fileExtension)) {
+            return res.status(400).json({
+                message: `The ${fileExtension} is not a valid file extension. Only allowed ${extentionsAllowed}`
+            });
+        }
+
+        const tempName = uuidv4() + '.' + fileExtension;
+        uploadPath = path.join(__dirname, '../uploads/', tempName);
 
         // Use the mv() method to place the file somewhere on your server
         fileUploaded.mv(uploadPath, function (err) {
@@ -28,10 +41,6 @@ export const uploadFile = async (req: Request, res: Response) => {
                 message: `File uploaded successfully to ${uploadPath}`
             });
         });
-
-        /* res.status(200).json({
-            message: 'File uploaded successfully'
-        }); */
     } catch (error) {
         console.log(error);
 
