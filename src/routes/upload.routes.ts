ad91012ba08191c81500e91e0526882ce8ff815d
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-05-24 16:54:30 -0500

    new validations applied

--- src/routes/upload.routes.ts
+++ src/routes/upload.routes.ts
@@ -1,7 +1,25 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
-import { uploadFile } from '../controllers/upload.controller';
+import { updateImage, uploadFile } from '../controllers/upload.controller';
+import { userValidation } from '../middlewares/user-validator';
+import { collectionsAllowed } from '../helpers/db-validator.helper';
+import { fileValidator } from '../middlewares/file-validator';
+
+const exitingCollections = ['users', 'products'];
 
 export const uploadsRoutes = Router();
 
-uploadsRoutes.post('/', uploadFile);
+uploadsRoutes.post('/', fileValidator, uploadFile);
+
+uploadsRoutes.put(
+    '/:collection/:id',
+    [
+        fileValidator,
+        check('id', 'No es un id válido de mongo').isMongoId(),
+        check('collection').custom((c) =>
+            collectionsAllowed(c, exitingCollections)
+        ),
+        userValidation
+    ],
+    updateImage
+);
