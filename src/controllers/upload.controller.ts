df748d131bc24dcba725ecb935ddaaeb3edf1abe
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-05-24 16:53:27 -0500

    new funcionality and refactor

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -1,18 +1,15 @@
 import { Response, Request } from 'express';
 import { processUploadFile } from '../helpers/upload-file.helper';
+import { UserModel } from '../models/user.model';
+import { ProductModel } from '../models/product.model';
+import fileUpload from 'express-fileupload';
 
 export const uploadFile = async (req: Request, res: Response) => {
     try {
-        if (
-            !req.files ||
-            Object.keys(req.files).length === 0 ||
-            !req.files.archivo
-        ) {
-            return res.status(400).send('No files were uploaded.');
-        }
+        const fileToUpload = req.files?.archivo as fileUpload.UploadedFile;
 
-        const fileName = await processUploadFile(req.files.archivo);
-        res.json({ name: fileName });
+        const fileUploadedName = await processUploadFile(fileToUpload);
+        res.json({ name: fileUploadedName });
     } catch (error) {
         console.log(error);
 
@@ -21,4 +18,45 @@ export const uploadFile = async (req: Request, res: Response) => {
             error
         });
     }
+};
+
+export const updateImage = async (req: Request, res: Response) => {
+    const { collection, id } = req.params;
+
+    let modelo;
+
+    switch (collection) {
+        case 'users':
+            modelo = await UserModel.findById(id);
+            if (!modelo)
+                return res.status(400).json({
+                    message: 'No existe el usuario'
+                });
+            break;
+        case 'products':
+            modelo = await ProductModel.findById(id);
+            if (!modelo)
+                return res.status(400).json({
+                    message: 'No existe el producto'
+                });
+            break;
+        default:
+            return res.status(500).json({
+                message: 'No valid collection'
+            });
+    }
+
+    const fileUploaded = req.files?.archivo as fileUpload.UploadedFile;
+
+    const fileName: any = await processUploadFile(
+        fileUploaded,
+        undefined,
+        collection
+    );
+
+    modelo.img = fileName;
+
+    await modelo.save();
+
+    res.json(modelo);
 };
