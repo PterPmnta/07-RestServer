f431fe4067e5badb83985b958b60b1b2c8912b24
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-05-24 17:21:32 -0500

    middleware refactored

--- src/middlewares/file-validator.ts
+++ src/middlewares/file-validator.ts
@@ -1,5 +1,9 @@
-export const fileValidator = (req: any, res: any, next: any) => {
-    if (!req.files || Object.keys(req.files).length === 0 || !req.files.file) {
+import { Request, Response } from 'express';
+
+export const fileValidator = (req: Request, res: Response, next: any) => {
+    const files = JSON.parse(JSON.stringify(req.files));
+
+    if (!files || Object.keys(files).length === 0 || !files.archivo) {
         return res.status(400).json({
             msg: 'No files to upload. Please check the file to upload.'
         });
