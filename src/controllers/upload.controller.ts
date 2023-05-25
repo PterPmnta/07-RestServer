b41475670b83307e8d74d63daf490faaf060308c
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-05-25 10:41:14 -0500

    refactorind and new func added

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -1,13 +1,18 @@
 import { Response, Request } from 'express';
 import path from 'path';
 import fs from 'fs';
+import fileUpload from 'express-fileupload';
+import { fileURLToPath } from 'url';
+import { v2 as cloudinary } from 'cloudinary';
 import { processUploadFile } from '../helpers/upload-file.helper';
 import { UserModel } from '../models/user.model';
 import { ProductModel } from '../models/product.model';
-import fileUpload from 'express-fileupload';
-import { fileURLToPath } from 'url';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+cloudinary.config(process.env.CLOUDINARY_URL!);
 
 export const uploadFile = async (req: Request, res: Response) => {
     try {
@@ -74,6 +79,57 @@ export const updateImage = async (req: Request, res: Response) => {
         }
 
         modelo.img = fileName;
+
+        await modelo.save();
+
+        res.json(modelo);
+    } catch (error) {
+        res.status(500).json({ msg: 'Error at the moment to upload the file' });
+    }
+};
+
+export const updateCloudinaryImage = async (req: Request, res: Response) => {
+    try {
+        const { collection, id } = req.params;
+
+        let modelo;
+
+        switch (collection) {
+            case 'users':
+                modelo = await UserModel.findById(id);
+                if (!modelo)
+                    return res.status(400).json({
+                        message: 'No existe el usuario'
+                    });
+                break;
+            case 'products':
+                modelo = await ProductModel.findById(id);
+                if (!modelo)
+                    return res.status(400).json({
+                        message: 'No existe el producto'
+                    });
+                break;
+            default:
+                return res.status(500).json({
+                    message: 'No valid collection'
+                });
+        }
+
+        if (modelo.img) {
+            const nameArr = modelo.img.split('/');
+            const name = nameArr[nameArr.length - 1];
+            const [public_id] = name.split('.');
+
+            await cloudinary.uploader.destroy(public_id);
+        }
+
+        const fileToUpload = req.files?.archivo as fileUpload.UploadedFile;
+
+        const respFromCloudinary = await cloudinary.uploader.upload(
+            fileToUpload.tempFilePath
+        );
+
+        modelo.img = respFromCloudinary.secure_url;
 
         await modelo.save();
 
